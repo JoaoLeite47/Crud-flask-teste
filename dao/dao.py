32ad7572fb78d33b5fce231cc6705bfbaa0ba7b3
JoaoLeite47 <StalkerJJ@outlook.com>
2022-08-07 18:45:56 -0300

    feat<resources> cors imports

--- dao/dao.py
+++ dao/dao.py
@@ -1,9 +1,4 @@
-# from flask_mysqldb import MySQL
-
-# app.config['MYSQL_HOST'] = 'localhost'
-# app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = ''
-# app.config['MYSQL_DB'] = 'locadora.db'
+from resources import *
 
 import mysql.connector
 from mysql.connector import Error
--- resources.py
+++ resources.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template, jsonify, url_for, redirect
 from flask import Flask, send_file
 import json
+from flask_cors import CORS
 
 
 app = Flask(__name__)  # Inicializa a aplicação
+CORS(app)
